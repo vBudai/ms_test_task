d7cfd8a484f31543163111b63cc8e718c67603f5
vbudai <vbudai297@gmail.com>
2025-07-23 19:42:06 +0500

    Установил symfony/test-pack и zenstruck/foundry

---
+++ src/Story/AppStory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Story;
+
+use App\Factory\ProductFactory;
+use Zenstruck\Foundry\Attribute\AsFixture;
+use Zenstruck\Foundry\Story;
+
+#[AsFixture(name: 'main')]
+final class AppStory extends Story
+{
+    public function build(): void
+    {
+        ProductFactory::createMany(10);
+    }
+}
